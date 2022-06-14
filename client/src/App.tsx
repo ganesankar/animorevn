6628fe4bd858f72da1a6b815d73fb6402a96c312
noname0204 <hoangductai2007@gmail.com>
2022-06-14 12:56:39 +0700

    initialize app

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className='flex h-screen items-center justify-center'>
+      <p className='text-5xl font-semibold text-red-500'>Hello World</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ server/src/index.ts
@@ -0,0 +1,5 @@
+const hello = 'Hello World';
+
+export const a: number = 1;
+
+console.log(hello);
