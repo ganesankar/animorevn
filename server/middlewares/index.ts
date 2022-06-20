f35f188e737b2910801099ffb8e72c65eacfd205
noname0204 <hoangductai2007@gmail.com>
2022-06-20 14:29:46 +0700

    server: fix middlewares file name

--- server/middlewares/index.ts
+++ server/middlewares/index.ts
@@ -1,4 +1,4 @@
-export { default as error404Handler } from './error404Handler.middleware';
-export { default as errorHandler } from './errorHandler.middleware';
-export { default as verifyAccessToken } from './verifyAccessToken.middleware';
-export { default as verifyRefreshToken } from './verifyRefreshToken.middleware';
+export { default as error404Handler } from './error404Handler';
+export { default as errorHandler } from './errorHandler';
+export { default as verifyAccessToken } from './verifyAccessToken';
+export { default as verifyRefreshToken } from './verifyRefreshToken';
